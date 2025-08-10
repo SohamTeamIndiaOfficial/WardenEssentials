865bbb71a3bb7012b9c6cb5867c4539fd4956349
SohamTeamIndiaOfficial <sohamteamindiaofficial@gmail.com>
2025-08-10 15:16:54 +0530

    Added new cmds

--- src/main/java/com/svhteam/lifesteal/wardenEssentials/WardenEssentials.java
+++ src/main/java/com/svhteam/lifesteal/wardenEssentials/WardenEssentials.java
@@ -17,8 +17,11 @@ import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Location;
+import org.bukkit.command.CommandExecutor;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class WardenEssentials extends JavaPlugin implements Listener {
@@ -69,6 +72,8 @@ public class WardenEssentials extends JavaPlugin implements Listener {
                     sender.sendMessage(ChatColor.AQUA + "/weunban <player>" + ChatColor.WHITE + " - Unban a player");
                     sender.sendMessage(ChatColor.AQUA + "/givewardenblade" + ChatColor.WHITE + " - Get the Warden Blade");
                     sender.sendMessage(ChatColor.AQUA + "/wessentials reload" + ChatColor.WHITE + " - Reload the config");
+                    sender.sendMessage(ChatColor.AQUA + "/wegive" + ChatColor.WHITE + " - Give an item to a player");
+                    sender.sendMessage(ChatColor.AQUA + "/wekill" + ChatColor.WHITE + " - Kill a player");
                     return true;
                 }
                 if (args[0].equalsIgnoreCase("reload")) {
@@ -117,15 +122,128 @@ public class WardenEssentials extends JavaPlugin implements Listener {
             return true;
         }
 
+        Player player = null;
         if (cmd.getName().equalsIgnoreCase("givewardenblade")) {
             if (!(sender instanceof Player)) {
                 sender.sendMessage(ChatColor.RED + "Only players can use this command!");
                 return true;
             }
-            Player player = (Player) sender;
+            player = (Player) sender;
             player.getInventory().addItem(createWardenBlade());
             player.sendMessage(ChatColor.GREEN + "You have been given the " + ChatColor.DARK_AQUA + "Warden Blade!");
             return true;
+        }
+
+        if (cmd.getName().equalsIgnoreCase("wekill")) {
+            if (!sender.hasPermission("wardenessentials.kill")) {
+                sender.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+                return true;
+            }
+
+            if (args.length < 1) {
+                sender.sendMessage(ChatColor.RED + "Usage: /wekill <player>");
+                return true;
+            }
+
+            Player target = Bukkit.getPlayerExact(args[0]);
+            if (target == null) {
+                sender.sendMessage(ChatColor.RED + "Player not found.");
+                return true;
+            }
+
+            target.setHealth(0.0);
+            sender.sendMessage(ChatColor.GREEN + "You have killed " + target.getName() + ".");
+            target.sendMessage(ChatColor.DARK_RED + "You were killed by" + sender.getName() + ".");
+            return true;
+        }
+
+        if (cmd.getName().equalsIgnoreCase("wegive")) {
+            if (!sender.hasPermission("wardenessentials.give")) {
+                sender.sendMessage(ChatColor.RED + "You don't have permission to use this command!");
+                return true;
+            }
+
+            if (args.length < 2) {
+                sender.sendMessage(ChatColor.RED + "Usage: /wegive <player> <item> [amount]");
+                return true;
+            }
+
+            Player target = Bukkit.getPlayerExact(args[0]);
+            if (target == null) {
+                sender.sendMessage(ChatColor.RED + "Player not found.");
+                return true;
+            }
+
+            int amount = 1;
+            if (args.length >= 3) {
+                try {
+                    amount = Integer.parseInt(args[2]);
+                } catch (NumberFormatException e) {
+                    sender.sendMessage(ChatColor.RED + "Invalid amount.");
+                    return true;
+                }
+            }
+
+            ItemStack item;
+
+            // Check for your custom "warden_blade"
+            if (args[1].equalsIgnoreCase("warden_blade")) {
+                item = new ItemStack(Material.DIAMOND_SWORD, amount);
+                ItemMeta meta = item.getItemMeta();
+                meta.setDisplayName(ChatColor.AQUA + "Warden Blade");
+                meta.setLore(Arrays.asList(ChatColor.GRAY + "Forged in the depths"));
+                meta.setCustomModelData(1); // Your texture's model data
+                item.setItemMeta(meta);
+            } else {
+                // Vanilla material
+                Material material = Material.matchMaterial(args[1]);
+                if (material == null) {
+                    sender.sendMessage(ChatColor.RED + "Invalid item: " + args[1]);
+                    return true;
+                }
+                item = new ItemStack(material, amount);
+            }
+
+            target.getInventory().addItem(item);
+
+            sender.sendMessage(ChatColor.GREEN + "Gave " + amount + "x " + item.getType().name() + " to " + target.getName() + ".");
+            target.sendMessage(ChatColor.GOLD + "You received " + amount + "x " + item.getType().name() + " from " + sender.getName() + ".");
+            return true;
+        }
+
+        if (cmd.getName().equalsIgnoreCase("weup")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(ChatColor.RED + "Only players can use this command!");
+                return true;
+            }
+
+            Player p =  (Player) sender;
+
+            if (!p.hasPermission("wardenessentials.up")) {
+                p.sendMessage(ChatColor.RED + "You do not have permission to use this command!");
+                return true;
+            }
+
+            int height = 1;
+            if (args.length >= 1) {
+                try {
+                    height = Integer.parseInt(args[0]);
+                } catch (NumberFormatException e) {
+                    p.sendMessage(ChatColor.RED + "Invalid height. Must be a number.");
+                    return true;
+                }
+            }
+
+            Location loc = p.getLocation();
+            Location newLoc = loc.clone().add(0, height, 0);
+
+            newLoc.getBlock().setType(Material.GLASS);
+
+            p.teleport(newLoc.add(0, 1, 0));
+
+            p.sendMessage(ChatColor.GREEN + "You have been lifted up " + height + " blocks!");
+            return true;
+
         }
 
         return false;
